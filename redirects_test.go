4047a40b362edae61992d4b0e494f9961150c92c
Justin Johnson <justin@justinjohnson.io>
2022-04-21 12:46:48 -0500

    Initial changes for fork

--- redirects_test.go
+++ redirects_test.go
@@ -3,31 +3,9 @@ package redirects_test
 import (
 	"testing"
 
+	redirects "github.com/justincjohnson/go-ipfs-redirects"
 	"github.com/tj/assert"
-	"github.com/tj/go-redirects"
 )
-
-func TestParams_Has(t *testing.T) {
-	p := redirects.Params{
-		"foo": true,
-		"bar": "baz",
-	}
-
-	assert.True(t, p.Has("foo"))
-	assert.True(t, p.Has("bar"))
-	assert.False(t, p.Has("baz"))
-}
-
-func TestParams_Get(t *testing.T) {
-	p := redirects.Params{
-		"foo": true,
-		"bar": "baz",
-	}
-
-	assert.Equal(t, true, p.Get("foo"))
-	assert.Equal(t, "baz", p.Get("bar"))
-	assert.Equal(t, nil, p.Get("baz"))
-}
 
 func TestRule_IsProxy(t *testing.T) {
 	t.Run("without host", func(t *testing.T) {
